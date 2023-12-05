c9f09c1922464b5d2a3c6ab469f0ff96c65125fe
GeekyROB96 <rohitrk9693@gmail.com>
2023-12-05 19:12:22 +0530

    Minimize the heights

---
+++ 05-12-23.java
@@ -0,0 +1,57 @@
+/*
+ * 
+ * Given an array arr[] denoting heights of N towers and a positive integer K.
+
+For each tower, you must perform exactly one of the following operations exactly once.
+
+Increase the height of the tower by K
+Decrease the height of the tower by K
+Find out the minimum possible difference between the height of the shortest and tallest towers after you have modified each tower.
+
+You can find a slight modification of the problem here.
+Note: It is compulsory to increase or decrease the height by K for each tower. After the operation, the resultant array should not contain any negative integers.
+
+Example 1:
+
+Input:
+K = 2, N = 4
+Arr[] = {1, 5, 8, 10}
+Output:
+5
+Explanation:
+The array can be modified as 
+{1+k, 5-k, 8-k, 10-k} = {3, 3, 6, 8}. 
+The difference between 
+the largest and the smallest is 8-3 = 5.
+
+ */
+
+
+
+ class Solution {
+    int getMinDiff(int[] arr, int n, int k) {
+        // code here
+        Arrays.sort(arr);
+        
+        int ans = arr[n-1] - arr[0];
+        
+        
+        
+        for(int i =1;i<n;i++){
+            
+            if(arr[i]>=k){
+                
+             int mine = Math.min(arr[i]-k,arr[0]+k);
+                
+            int maxe = Math.max(arr[i-1]+k,arr[n-1]-k);
+            
+            
+             
+            
+            ans = Math.min(ans,maxe-mine);
+            }
+        }
+        
+        return ans;
+    }
+}
